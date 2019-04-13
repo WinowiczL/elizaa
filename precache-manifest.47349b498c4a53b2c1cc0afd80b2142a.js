b05d9a72603174c1afbb1c40ca4b4ecc2f895798
Lukasz Winowicz <winowiczlukasz@gmail.com>
2019-04-13 16:06:05 +0200

    Updates

---
+++ precache-manifest.47349b498c4a53b2c1cc0afd80b2142a.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe619b2edf2cfdb58f1bcce3d9a6828a",
+    "url": "/elizaa/static/media/1.fe619b2e.jpg"
+  },
+  {
+    "revision": "061e327afc2d2a15af0a",
+    "url": "/elizaa/static/css/main.cbeaa5ca.chunk.css"
+  },
+  {
+    "revision": "5e08bfef354c30e1e6d6",
+    "url": "/elizaa/static/js/runtime~main.cd33c562.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/elizaa/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c9dd62f1231eb9fbca23",
+    "url": "/elizaa/static/js/2.6a6f7fce.chunk.js"
+  },
+  {
+    "revision": "0232b53a2d4ad921d3eb681b180a1c7c",
+    "url": "/elizaa/static/media/1.0232b53a.jpg"
+  },
+  {
+    "revision": "7c06ab9ac852826abf6d4cba032166cf",
+    "url": "/elizaa/static/media/2.7c06ab9a.jpg"
+  },
+  {
+    "revision": "c6e5c84c580da1054fbe3dae61b853b4",
+    "url": "/elizaa/static/media/3.c6e5c84c.jpg"
+  },
+  {
+    "revision": "8bb0ea82fa31adc246bd39e775559da2",
+    "url": "/elizaa/static/media/eliza.8bb0ea82.jpg"
+  },
+  {
+    "revision": "c750d242fafc6932ee197bfb5b6182c1",
+    "url": "/elizaa/static/media/ciazowa.c750d242.jpg"
+  },
+  {
+    "revision": "9d3c443ab8709dc671ac874ea2aa2a78",
+    "url": "/elizaa/static/media/dziecieca.9d3c443a.jpg"
+  },
+  {
+    "revision": "68c81f5162a4355d0d972b35cefc8394",
+    "url": "/elizaa/static/media/noworodkowa.68c81f51.jpg"
+  },
+  {
+    "revision": "1f2a9f724ce7786691da7daf59a6aba8",
+    "url": "/elizaa/static/media/portretowa.1f2a9f72.jpg"
+  },
+  {
+    "revision": "92e268bfa041261b3d843a2e43dae27a",
+    "url": "/elizaa/static/media/rodzinna.92e268bf.jpg"
+  },
+  {
+    "revision": "cc2df10211991f705c24be27a671ffd1",
+    "url": "/elizaa/static/media/slubna.cc2df102.jpg"
+  },
+  {
+    "revision": "5b9a6cbe856f132587a9c69a502b5ce8",
+    "url": "/elizaa/static/media/logoWelcome.5b9a6cbe.jpg"
+  },
+  {
+    "revision": "9f0d9e73f2ccbb9f67df9469dbb661a4",
+    "url": "/elizaa/static/media/1.9f0d9e73.jpg"
+  },
+  {
+    "revision": "445666a2e2408df01052b002eb51b775",
+    "url": "/elizaa/static/media/2.445666a2.jpg"
+  },
+  {
+    "revision": "5cda36baddf6080dca7c6bb687d34d21",
+    "url": "/elizaa/static/media/3.5cda36ba.jpg"
+  },
+  {
+    "revision": "be8ab618cb357d8c6d8fbb16733c28a0",
+    "url": "/elizaa/static/media/4.be8ab618.jpg"
+  },
+  {
+    "revision": "6e6eeaf030475e2396fde8f7097f76b8",
+    "url": "/elizaa/static/media/5.6e6eeaf0.jpg"
+  },
+  {
+    "revision": "5bb3710f0245b53a5b6a3e1b0f4af93f",
+    "url": "/elizaa/static/media/1.5bb3710f.jpg"
+  },
+  {
+    "revision": "09672c1ea9d0000a086196b01583206a",
+    "url": "/elizaa/static/media/2.09672c1e.jpg"
+  },
+  {
+    "revision": "8e1f108350c6e98a72b53db3420224c0",
+    "url": "/elizaa/static/media/4.8e1f1083.jpg"
+  },
+  {
+    "revision": "0232b53a2d4ad921d3eb681b180a1c7c",
+    "url": "/elizaa/static/media/5.0232b53a.jpg"
+  },
+  {
+    "revision": "061e327afc2d2a15af0a",
+    "url": "/elizaa/static/js/main.3b2ae68a.chunk.js"
+  },
+  {
+    "revision": "80f75d2a53aa1de0974c9f0c8c816a55",
+    "url": "/elizaa/static/media/2.80f75d2a.jpg"
+  },
+  {
+    "revision": "a4dbaf9ee93e7ded7fa2d846e9bd4e6c",
+    "url": "/elizaa/static/media/3.a4dbaf9e.jpg"
+  },
+  {
+    "revision": "c750d242fafc6932ee197bfb5b6182c1",
+    "url": "/elizaa/static/media/4.c750d242.jpg"
+  },
+  {
+    "revision": "7711ab73c6ff69827d35ca5a7f5f2ac4",
+    "url": "/elizaa/static/media/5.7711ab73.jpg"
+  },
+  {
+    "revision": "58990771fc44ab87127c6b60ab0503c4",
+    "url": "/elizaa/static/media/1.58990771.jpg"
+  },
+  {
+    "revision": "c59edd6d77b30465aab9dc64faf827fe",
+    "url": "/elizaa/static/media/2.c59edd6d.jpg"
+  },
+  {
+    "revision": "7a48cc8ee07109975806917f9445b8fc",
+    "url": "/elizaa/static/media/3.7a48cc8e.jpg"
+  },
+  {
+    "revision": "5ff3f868c6e74e28b125ed9b53a997b8",
+    "url": "/elizaa/static/media/4.5ff3f868.jpg"
+  },
+  {
+    "revision": "f7d6c9c6907cd53c8ee47676742af49a",
+    "url": "/elizaa/static/media/5.f7d6c9c6.jpg"
+  },
+  {
+    "revision": "7fb6ae89f8497b46474959f9216623c7",
+    "url": "/elizaa/static/media/1.7fb6ae89.jpg"
+  },
+  {
+    "revision": "ad4891054df6a4348251cc262009c737",
+    "url": "/elizaa/static/media/2.ad489105.jpg"
+  },
+  {
+    "revision": "5c57acac4b7a7a040ff5232821a534eb",
+    "url": "/elizaa/static/media/3.5c57acac.jpg"
+  },
+  {
+    "revision": "92e268bfa041261b3d843a2e43dae27a",
+    "url": "/elizaa/static/media/4.92e268bf.jpg"
+  },
+  {
+    "revision": "c866e10594a00894474123bd0a826646",
+    "url": "/elizaa/static/media/5.c866e105.jpg"
+  },
+  {
+    "revision": "bfd6bd7d042bb8bc8bebc6b7a77200d7",
+    "url": "/elizaa/static/media/1.bfd6bd7d.jpg"
+  },
+  {
+    "revision": "c36b8ccaab2e55ca573a9bbd0fcf2fa2",
+    "url": "/elizaa/static/media/2.c36b8cca.jpg"
+  },
+  {
+    "revision": "10db37dd40f269b748ed584693a37ce5",
+    "url": "/elizaa/static/media/3.10db37dd.jpg"
+  },
+  {
+    "revision": "447b25e4acdfaf1e87d8207846e60240",
+    "url": "/elizaa/static/media/4.447b25e4.jpg"
+  },
+  {
+    "revision": "12e35648a770a273810478f54def5374",
+    "url": "/elizaa/static/media/5.12e35648.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/elizaa/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/elizaa/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/elizaa/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "c9dd62f1231eb9fbca23",
+    "url": "/elizaa/static/css/2.1ea57659.chunk.css"
+  },
+  {
+    "revision": "d330f03ed4486d1d8498fc9dbf3b9db8",
+    "url": "/elizaa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/elizaa/precache-manifest.f4348e3f5aeaad63abe6f89017e83d42.js"
+  "/elizaa/precache-manifest.47349b498c4a53b2c1cc0afd80b2142a.js"
 );
 
 workbox.clientsClaim();
